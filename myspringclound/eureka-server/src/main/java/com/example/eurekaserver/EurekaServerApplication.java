22e842a9288be64252ad69a64ef2b94536c62699
gujian1992 <903495291@qq.com>
2019-05-08 10:50:00 +0800

    update

--- myspringclound/eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ myspringclound/eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -10,6 +10,7 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+        System.out.println("注册中心启动了");
     }
 
 }
